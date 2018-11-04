c347936ee9b79414e85b82c45da35d3873b6316e
Jesse Kolb <jessefkolb@gmail.com>
2018-11-04 16:45:11 -0500

    bug fix
    fixed another menu bug

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour {
 
@@ -60,6 +61,6 @@ public class PauseMenu : MonoBehaviour {
 
     public void Quit()
     {
-        Application.Quit();
+        SceneManager.LoadScene("Menu");
     }
 }
